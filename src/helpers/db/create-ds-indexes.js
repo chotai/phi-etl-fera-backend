3b76017f980bc853550eab63476562d6dc9a4927
SajuJ <162431432+SajuJ@users.noreply.github.com>
2024-05-29 18:03:42 +0100

    commented collection.createIndex which was resulting in exception.

--- src/helpers/db/create-ds-indexes.js
+++ src/helpers/db/create-ds-indexes.js
@@ -9,17 +9,15 @@ async function createMongoDBIndexes(db) {
     collections?.forEach(async (collectionInfo) => {
       // TODO: Check when and how often Index need to be create/refreshed
       // Check for Plant_Detail collection
-      if (collectionInfo.name === 'PLANT_DETAIL') {
-        const collection = db.collection(collectionInfo.name)
-
-        // Create an index on the specified field
-        // TODO: CREATE INDEX IS THROWING MONGODB EXECPTION. COMMENTING IT TO LOAD
-        // THE LATEST RECORDS. INDEXS ARE REQUIRED ON DB, THIS ISSUE NEED TO BE LOOKED
-        // INTO AND RESOLVED.
-        //await collection.createIndex({ LATIN_NAME: 1 })
-
-        // TODO: Create Index on multiple fields (if required)
-      }
+      // if (collectionInfo.name === 'PLANT_DETAIL') {
+      // const collection = db.collection(collectionInfo.name)
+      // Create an index on the specified field
+      // TODO: CREATE INDEX IS THROWING MONGODB EXECPTION. COMMENTING IT TO LOAD
+      // THE LATEST RECORDS. INDEXS ARE REQUIRED ON DB, THIS ISSUE NEED TO BE LOOKED
+      // INTO AND RESOLVED.
+      // await collection.createIndex({ LATIN_NAME: 1 })
+      // TODO: Create Index on multiple fields (if required)
+      // }
     })
   } catch (error) {
     logger.error('Could not create MongoDB indexes', error)
